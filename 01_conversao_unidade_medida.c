ba8bf6aa9384ad1501e9c313c040ec97eee33b05
Vianna07 <viannagustavo71@gmail.com>
2025-03-30 21:48:46 -0300

    formata código com clang-format

--- 01_conversao_unidade_medida.c
+++ 01_conversao_unidade_medida.c
@@ -1,31 +1,30 @@
 #include <stdio.h>
 
 int main() {
-    float pes, polegadas, jardas, milhas;
+  float pes, polegadas, jardas, milhas;
 
-    /*
-    * Considere:
-    * 1 pé = 12 polegadas
-    * 1 jarda = 3 pés
-    * 1 milha = 1760 jardas
-    */
+  /*
+   * Considere:
+   * 1 pé = 12 polegadas
+   * 1 jarda = 3 pés
+   * 1 milha = 1760 jardas
+   */
 
-    printf("\nQual a medida em pes?: ");
-    scanf("%f", &pes);
-    
-    if (pes < 0) {
-    	printf("\nValor nao pode ser negativo!");	
-    	return 0;
-	}
+  printf("\nQual a medida em pes?: ");
+  scanf("%f", &pes);
 
-    polegadas = pes * 12;
-    jardas = pes * 3;
-    milhas = jardas / 1760;
-
-    printf("\n\nEm polegadas: %f", polegadas);
-    printf("\n\nEm jardas: %f", jardas);
-    printf("\n\nEm milhas: %f", milhas);
-
+  if (pes < 0) {
+    printf("\nValor nao pode ser negativo!");
     return 0;
-}
+  }
 
+  polegadas = pes * 12;
+  jardas = pes * 3;
+  milhas = jardas / 1760;
+
+  printf("\n\nEm polegadas: %f", polegadas);
+  printf("\nEm jardas: %f", jardas);
+  printf("\nEm milhas: %f", milhas);
+
+  return 0;
+}
